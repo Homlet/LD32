2bd4ff69480826a86b4ca09b4c5484810ce1f09a
Homletmoo <samlhub@gmail.com>
2015-04-17 23:09:06 +0100

    Initial commit.

---
+++ Assets/Homletmoo/Scripts/Generic/Advance.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Advance : MonoBehaviour
+{
+    int loadedLevel;
+    int levelCount;
+
+    public void Awake()
+    {
+        loadedLevel = Application.loadedLevel;
+        levelCount = Application.levelCount;
+    }
+
+    public void AdvanceScene()
+    {
+        Application.LoadLevel((loadedLevel + 1) % levelCount);
+    }
+}
