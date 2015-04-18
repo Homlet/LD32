3fa7e3535271842c4562b2bbc9871fcbaa878346
Homletmoo <samlhub@gmail.com>
2015-04-18 14:34:18 +0100

    Add aeroplane.

---
+++ Assets/Homletmoo/Scripts/Generic/Follow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour
+{
+    public Transform parent;
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        transform.position = parent.position + parent.TransformDirection(offset);
+    }
+}
---
+++ Assets/PlaneMovement.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneMovement : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position += new Vector3(Time.deltaTime * 300, 0);
+    }
+}
